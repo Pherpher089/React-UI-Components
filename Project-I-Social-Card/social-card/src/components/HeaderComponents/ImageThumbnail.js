aba91da78140d8fac63b54f5a3f9ad8fa0c17dd4
Pherpher089 <Christutor089@gmail.com>
2019-05-08 19:52:55 -0700

    Got pretty deep into the social card project. Forgot to commit frequently...

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = ()=>{
+    return(
+        <div>
+            <img src = {require ('./lambdacrest.png')} alt = 'This is alt'/>
+        </div>
+    );
+};
+
+export default ImageThumbnail;
