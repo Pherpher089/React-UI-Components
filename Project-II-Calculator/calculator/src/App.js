5ae844e48575457d50090686c93ca7c629869fcb
Pherpher089 <Christutor089@gmail.com>
2019-05-10 07:20:24 -0700

    Set up the buttons on the calculator

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,48 @@
 import React from 'react';
 import './App.css';
 
+import Display from './components/DisplayComponents/CalculatorDisplay.js';
+import NumberButton from './components/ButtonComponents/NumberButton.js';
+import ActionButtons from './components/ButtonComponents/ActionButton.js';
+
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className = 'App'>
+      <Display />
+      <div className = 'Buttons'>
+        <div className = 'Number-Buttons'>
+          <div className = 'Clear-Button'>
+            <ActionButtons/>
+          </div> 
+          <div className = 'Number-Row'>
+            <NumberButton/>
+            <NumberButton/>
+            <NumberButton/>
+          </div>
+          <div className = 'Number-Row'>
+            <NumberButton/>
+            <NumberButton/>
+            <NumberButton/>
+          </div>
+
+          <div className = 'Number-Row'>
+            <NumberButton/>
+            <NumberButton/>
+            <NumberButton/>
+          </div>
+          <div className = 'Zero-Button'>
+            <NumberButton/>
+          </div>  
+        </div>
+        <div className = 'ActionButtons'> 
+          <ActionButtons/>
+          <ActionButtons/>
+          <ActionButtons/>
+          <ActionButtons/>
+          <ActionButtons/>
+        </div>
+      </div>
+     
     </div>
   );
 };
--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = ()=>{
+    return(
+        <button>Action Button</button>
+    );
+}
+
+export default ActionButton;
