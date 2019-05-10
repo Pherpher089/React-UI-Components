4541e1f8d5dcd08d18d441fab721bb9105600c82
Pherpher089 <Christutor089@gmail.com>
2019-05-10 04:22:59 -0700

    Finished the first project MVP. Working on adding in icons for footer

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = ()=>{
+    return(
+        <div className = "Footer">
+            <img src = {required ("")} />
+            <img src = {required ("")} />
+            <img src = {required ("")} />
+            <img src = {required ("")} />
+        </div>
+    );
+}
+
+export default Footer;
